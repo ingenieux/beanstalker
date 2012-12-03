48433b03edfdf469b2d46e9353db833277bbe88c
Aldrin Leal <aldrin@leal.eng.br>
2012-12-03 15:29:41 -0300

    Improving Fast-Deploy
    --HG--
    branch : fastdeploy

--- beanstalk-maven-plugin/src/main/java/br/com/ingenieux/mojo/beanstalk/bundle/FastDeployMojo.java
+++ beanstalk-maven-plugin/src/main/java/br/com/ingenieux/mojo/beanstalk/bundle/FastDeployMojo.java
@@ -4,12 +4,15 @@ import static org.apache.commons.lang.StringUtils.defaultIfBlank;
 
 import java.io.File;
 import java.util.Date;
+import java.util.ListIterator;
 
 import org.eclipse.jgit.api.AddCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.PushCommand;
 import org.eclipse.jgit.api.Status;
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RepositoryBuilder;
 import org.eclipse.jgit.lib.TextProgressMonitor;
 import org.eclipse.jgit.transport.RefSpec;
 import org.jfrog.maven.annomojo.annotations.MojoGoal;
@@ -17,6 +20,10 @@ import org.jfrog.maven.annomojo.annotations.MojoParameter;
 import org.jfrog.maven.annomojo.annotations.MojoSince;
 
 import br.com.ingenieux.mojo.beanstalk.AbstractNeedsEnvironmentMojo;
+
+import com.amazonaws.services.elasticbeanstalk.model.ApplicationVersionDescription;
+import com.amazonaws.services.elasticbeanstalk.model.DescribeApplicationVersionsRequest;
+import com.amazonaws.services.elasticbeanstalk.model.DescribeApplicationVersionsResult;
 
 /**
  * Uploads a packed war file to Amazon S3 for further Deployment.
@@ -30,6 +37,12 @@ public class FastDeployMojo extends AbstractNeedsEnvironmentMojo {
 	@MojoParameter(expression = "${project.build.directory}/${project.build.finalName}")
 	File sourceDirectory;
 
+	@MojoParameter(expression = "${beanstalk.stagingDirectory}", description = "Git Staging Dir (should not be under target/)", defaultValue = "${project.basedir}/tmp-git-deployment-staging")
+	File stagingDirectory;
+
+	@MojoParameter(expression = "${beanstalk.useStagingDirectory}", description = "Use Staging Directory?", defaultValue = "false")
+	boolean useStagingDirectory = false;
+
 	@Override
 	protected void configure() {
 		try {
@@ -42,15 +55,7 @@ public class FastDeployMojo extends AbstractNeedsEnvironmentMojo {
 
 	@Override
 	protected Object executeInternal() throws Exception {
-		File gitRepo = new File(sourceDirectory, ".git");
-
-		Git git = null;
-
-		if (!gitRepo.exists()) {
-			git = Git.init().setDirectory(sourceDirectory).call();
-		} else {
-			git = Git.open(gitRepo);
-		}
+		Git git = getGitRepo();
 
 		Status status = git.status().call();
 
@@ -59,12 +64,12 @@ public class FastDeployMojo extends AbstractNeedsEnvironmentMojo {
 
 			return null;
 		}
-		
+
 		// Asks for Existing Files to get added
 		git.add().setUpdate(true).addFilepattern(".").call();
 
 		// Now as for any new files (untracked)
-		
+
 		AddCommand addCommand = git.add();
 
 		if (!status.getUntracked().isEmpty()) {
@@ -103,10 +108,65 @@ public class FastDeployMojo extends AbstractNeedsEnvironmentMojo {
 			// Ignore
 		}
 
-		String gitVersion = "git-" + commitId;
+		String applicationVersionId = null;
 
-		getLog().info("Deployed version " + gitVersion);
+		{
+			String gitVersionBase = "git-" + commitId;
+			DescribeApplicationVersionsResult describeApplicationVersions = getService()
+					.describeApplicationVersions(
+							new DescribeApplicationVersionsRequest()
+									.withApplicationName(applicationName));
+			boolean found = false;
+
+			ListIterator<ApplicationVersionDescription> versions = describeApplicationVersions
+					.getApplicationVersions().listIterator();
+			
+			while (! found) {
+				ApplicationVersionDescription curVer = versions.next();
+				
+				found = curVer.getVersionLabel().startsWith(gitVersionBase);
+				
+				if (found) {
+					applicationVersionId = curVer.getVersionLabel();
+				}
+			}
+		}
+
+		getLog().info("Deployed version " + applicationVersionId);
 
 		return null;
+	}
+
+	private Git getGitRepo() throws Exception {
+		Git git = null;
+		
+		if (!useStagingDirectory) {
+			File gitRepo = new File(sourceDirectory, ".git");
+
+			if (!gitRepo.exists()) {
+				git = Git.init().setDirectory(sourceDirectory).call();
+			} else {
+				git = Git.open(gitRepo);
+			}
+		} else {
+			File gitRepo = stagingDirectory;
+			Repository r = null;
+			
+			RepositoryBuilder b = new RepositoryBuilder().setGitDir(stagingDirectory).setWorkTree(sourceDirectory);
+
+			if (!gitRepo.exists()) {
+				gitRepo.getParentFile().mkdirs();
+				
+				r = b.build();
+				
+				r.create();
+			} else {
+				r = b.build();
+			}
+			
+			git = Git.wrap(r);
+		}
+		
+		return git;
 	}
 }
