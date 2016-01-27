d91f7c60b3b0606ba883bc155ced9f7ef5a16753
Wilmoth, Jon <jon.d.wilmoth@nordstrom.com>
2016-01-27 09:27:34 -0800

    Address Environments created with mixed case Cnames prefixes.

--- beanstalk-maven-plugin/src/main/java/br/com/ingenieux/mojo/beanstalk/util/EnvironmentHostnameUtil.java
+++ beanstalk-maven-plugin/src/main/java/br/com/ingenieux/mojo/beanstalk/util/EnvironmentHostnameUtil.java
@@ -3,8 +3,6 @@ package br.com.ingenieux.mojo.beanstalk.util;
 import com.amazonaws.regions.Region;
 import com.amazonaws.services.elasticbeanstalk.model.EnvironmentDescription;
 import com.google.common.base.Predicate;
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.Validate;
 
 import java.util.Set;
 import java.util.TreeSet;
@@ -12,6 +10,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static java.lang.String.format;
+import static org.apache.commons.lang.StringUtils.join;
 
 public class EnvironmentHostnameUtil {
     public static final Pattern PATTERN_HOSTNAME = Pattern.compile("(?<cnamePrefix>[\\p{Alnum}\\-]{4,63})(?<regionName>.\\p{Alpha}{2}\\-\\p{Alpha}{4,9}\\-\\p{Digit})?\\Q.elasticbeanstalk.com\\E$");
@@ -25,12 +24,12 @@ public class EnvironmentHostnameUtil {
         return new Predicate<EnvironmentDescription>() {
             @Override
             public boolean apply(EnvironmentDescription t) {
-                return hostnamesToMatch.contains(t.getCNAME());
+                return hostnamesToMatch.contains(t.getCNAME().toLowerCase());
             }
 
             @Override
             public String toString() {
-                return format("... with cname belonging to %s", StringUtils.join(hostnamesToMatch.iterator(), " or "));
+                return format("... with cname matching (case-insensitive) %s", join(hostnamesToMatch.iterator(), " or "));
             }
         };
     }
